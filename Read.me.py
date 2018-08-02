9ac5c6621922d9b25793cbeda3e1888dcc5e3647
SeanThompson1 <sean.thompson1@live.com>
2018-08-02 15:08:08 +0100

    Added Read.me.txt

---
+++ Read.me.py
@@ -0,0 +1,28 @@
+
+#Input Username
+userName = input("Hi Whats your name? ")
+passwordAttempt = input("Please enter your password. ")
+
+#Correct password
+password = "password"
+
+if passwordAttempt == password:
+    print("Hi " + userName)
+    input("What " + "is " + "your " + "age? ")
+   
+    number_one = input("Try entering a number. ")
+    number_two = input("Enter another number. ")
+    number_one = int(number_one)
+    number_two = int(number_two)
+
+    total = number_one + number_two
+
+    print("The sum of these 2 numbers is: " + str(total))
+
+    if total == 69 or 420:
+        while True:
+            print("Nice Meme")
+
+#Incorrect Password      
+elif passwordAttempt != password:
+    print("Please try that again. ")
